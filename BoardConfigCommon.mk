01ca6cceca265ea802d776502cf813672883261e
ChrisW444 <wandersonrodriguesf1@gmail.com>
2022-02-11 21:31:50 +0000

    sm8350-common: Drop Lineage stuff
    [1] - powershare is not compiling but for some bizarre reason,
           if we drop its folder we get bootloop in splash.
    
    Signed-off-by: ChrisW444 <wandersonrodriguesf1@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,8 +123,8 @@ BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Media
